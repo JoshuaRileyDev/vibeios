afb3d7f8a4f3b20e42df591141fded1c5bd03503
Joshua Riley <joshua@joshuariley.co.uk>
2025-06-16 16:02:48 +0100

    Initial Commit

---
+++ vibeios/vibeiosApp.swift
@@ -0,0 +1,17 @@
+//
+//  vibeiosApp.swift
+//  vibeios
+//
+//  Created by Joshua Riley on 16/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct vibeiosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
